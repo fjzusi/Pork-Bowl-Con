c46eb1be89b74e5593f6c1f85a6d8f53e71a0ad7
fjzusi <zidane320@yahoo.com>
2017-02-06 14:29:43 -0500

    Added icon export script

---
+++ img/icon/export.bat
@@ -0,0 +1,8 @@
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 32 -w 32 -e 32x32.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 57 -w 57 -e 57x57.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 76 -w 76 -e 76x76.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 120 -w 120 -e 120x120.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 128 -w 128 -e 128x128.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 152 -w 152 -e 152x152.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 180 -w 180 -e 180x180.png
+"C:\Program Files (x86)\Inkscape\inkscape.exe" icon.svg -h 192 -w 192 -e 192x192.png
